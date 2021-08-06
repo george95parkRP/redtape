6c138cfb1530507afa6ac7c67f9d59ff42c45df1
George Park <george.park@radpartners.com>
2021-08-06 08:18:44 -0700

    Ent schemas.

---
+++ sqlmanager/ent/schema/policyoptions.go
@@ -0,0 +1,33 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// PolicyOptions holds the schema definition for the PolicyOptions entity.
+type PolicyOptions struct {
+	ent.Schema
+}
+
+// Fields of the PolicyOptions.
+func (PolicyOptions) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id"),
+		field.String("name"),
+		field.String("description"),
+		field.Strings("resources"),
+		field.Strings("actions"),
+		field.Strings("scopes"),
+		field.String("effect"),
+	}
+}
+
+// Edges of the PolicyOptions.
+func (PolicyOptions) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("roles", Roles.Type),
+		edge.To("conditions", Conditions.Type),
+	}
+}
