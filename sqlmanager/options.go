8060fe2db584ded621bc340ce57672c73ba1f2c4
George Park <george.park@radpartners.com>
2021-08-06 08:19:25 -0700

    New sql manager options.

---
+++ sqlmanager/options.go
@@ -0,0 +1,33 @@
+package sqlmanager
+
+type SqlManagerOptions struct {
+	Dialect    string
+	ConnString string
+}
+
+type SqlManagerOption func(*SqlManagerOptions)
+
+// Returns a new SqlManageOptions object after applying options.
+func NewSqlManagerOptions(opts ...SqlManagerOption) SqlManagerOptions {
+	options := SqlManagerOptions{}
+
+	for _, o := range opts {
+		o(&options)
+	}
+
+	return options
+}
+
+// SetDialect sets the SqlManager dialect option.
+func SetDialect(d string) SqlManagerOption {
+	return func(o *SqlManagerOptions) {
+		o.Dialect = d
+	}
+}
+
+// SetDialect sets the SqlManager connstring option.
+func SetConnString(conn string) SqlManagerOption {
+	return func(o *SqlManagerOptions) {
+		o.ConnString = conn
+	}
+}
