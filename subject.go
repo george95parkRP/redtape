d6da4da85e2171f286cd59ef838b38d6233fc902
George Park <george.park@radpartners.com>
2021-08-27 13:16:27 -0700

    removed comments and added withconditionregistry option.

--- subject.go
+++ subject.go
@@ -1,43 +1,26 @@
 package redtape
 
+import "github.com/blushft/redtape/match"
+
 type Subject interface {
 	Type() string
-	// Metadata() map[string]interface{}
-	// Enforcer() EnforceFunc
 	Conditions() Conditions
-	// EffectiveRoles() []*role.Role
-	// MatchRole(string) bool
+	MatchSubject(string) bool
 }
 
 type subject struct {
-	typ string
-	// roles      []*role.Role
-	// meta       map[string]interface{}
-	// enforcer   EnforceFunc
+	typ        string
 	conditions Conditions
 }
-
-// func (s *subject) Enforce(req *Request) error {
-// 	for _, r := range s.EffectiveRoles() {
-// 		if r.ID == "test" {
-// 			return nil
-// 		}
-// 	}
-
-// 	return NewErrRequestDeniedImplicit(errors.New("access denied because no roles match assignement"))
-// }
 
 func NewSubject(typ string, opts ...SubjectOption) (Subject, error) {
 	options := NewSubjectOptions(opts...)
 
 	sub := &subject{
 		typ: typ,
-		// roles:    options.Roles,
-		// meta:     options.Meta,
-		// enforcer: options.Enforcer,
 	}
 
-	conds, err := NewConditions(options.Conditions, nil)
+	conds, err := NewConditions(options.Conditions, options.ConditionRegistry)
 	if err != nil {
 		return nil, err
 	}
@@ -51,42 +34,21 @@ func (s *subject) Type() string {
 	return s.typ
 }
 
-// func (s *subject) Metadata() map[string]interface{} {
-// 	return s.meta
-// }
-
-// func (s *subject) Enforcer() EnforceFunc {
-// 	return s.enforcer
-// }
-
 func (s *subject) Conditions() Conditions {
 	return s.conditions
 }
 
-// func (s *subject) EffectiveRoles() []*role.Role {
-// 	var er []*role.Role
-// 	for _, r := range s.roles {
-// 		er = append(er, r.EffectiveRoles()...)
-// 	}
+func (s *subject) MatchSubject(val string) bool {
+	if match.Wildcard(s.typ, val) {
+		return true
+	}
 
-// 	return er
-// }
-
-// func (s *subject) MatchRole(roleID string) bool {
-// 	for _, r := range s.EffectiveRoles() {
-// 		if match.Wildcard(roleID, r.ID) {
-// 			return true
-// 		}
-// 	}
-
-// 	return false
-// }
+	return false
+}
 
 type SubjectOptions struct {
-	// Enforcer   EnforceFunc
-	// Meta       map[string]interface{}
-	// Roles      []*role.Role
-	Conditions []ConditionOptions
+	Conditions        []ConditionOptions
+	ConditionRegistry ConditionRegistry
 }
 
 type SubjectOption func(*SubjectOptions)
@@ -98,41 +60,17 @@ func NewSubjectOptions(opts ...SubjectOption) SubjectOptions {
 		o(&options)
 	}
 
-	// if options.Meta == nil {
-	// 	options.Meta = make(map[string]interface{})
-	// }
-
 	return options
 }
 
-// func SubjectEnforcer(e EnforceFunc) SubjectOption {
-// 	return func(o *SubjectOptions) {
-// 		o.Enforcer = e
-// 	}
-// }
-
-// func SubjectMeta(meta ...map[string]interface{}) SubjectOption {
-// 	return func(o *SubjectOptions) {
-// 		if o.Meta == nil {
-// 			o.Meta = make(map[string]interface{})
-// 		}
-
-// 		for _, md := range meta {
-// 			for k, v := range md {
-// 				o.Meta[k] = v
-// 			}
-// 		}
-// 	}
-// }
-
-// func SubjectRole(role ...*role.Role) SubjectOption {
-// 	return func(o *SubjectOptions) {
-// 		o.Roles = append(o.Roles, role...)
-// 	}
-// }
-
 func WithConditions(co ...ConditionOptions) SubjectOption {
 	return func(o *SubjectOptions) {
-		o.Conditions = append(o.Conditions, co...)
+		o.Conditions = co
+	}
+}
+
+func WithConditionRegistry(cr ConditionRegistry) SubjectOption {
+	return func(o *SubjectOptions) {
+		o.ConditionRegistry = cr
 	}
 }
